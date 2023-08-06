5da5017006abafbb4b77de6d63ab193f49b60de8
OUDOUROUCH-Youssef <youdourouch@gmail.com>
2023-08-07 07:25:00 +1300

    code source

--- src/components/upload.js
+++ src/components/upload.js
@@ -1,13 +1,41 @@
-import React from 'react';
+
 import'../App';
+import React, { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF, Stage, PresentationControls } from '@react-three/drei';
+
+function Model({ url, ...props }) {
+  const { scene } = useGLTF(url);
+  return <primitive object={scene} {...props} />;
+}
+
 function Upload() {
-  return (            
-            <div className="upload_base">
-              <label htmlFor="fileInput" className="file-upload-button">
+  const [uploadedModel, setUploadedModel] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setUploadedModel(url);
+    }
+  };
+
+  return ( 
+    <div className="upload_base">
+      <label htmlFor="fileInput" className="file-upload-button">
                 Upload File
-              </label>
-              <input type="file" id="fileInput" style={{ display:'block' }} />
-            </div>
+      </label>
+      <input type="file" accept=".gltf, .glb, .obj, .fbx" onChange={handleFileUpload} />
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: 'absolute', width: '50%', height: '82%' }}>
+        <color attach="background" args={['#ADD8E6']} />
+        <PresentationControls speed={1.5} global zoom={0.5} polar={[-0.1, Math.PI / 4]}>
+          <Stage environment={'sunset'}>
+            {uploadedModel ? <Model url={uploadedModel} scale={0.01} /> : null}
+          </Stage>
+        </PresentationControls>
+      </Canvas>
+    </div>
   );
-  }
+}
+  
 export default Upload;
